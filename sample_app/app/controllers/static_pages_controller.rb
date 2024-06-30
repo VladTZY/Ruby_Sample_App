ab73b7ca705e134685d088ad3e336006b5db0877
VladTZY <tiganilavlad@gmail.com>
2024-06-30 18:00:10 +0300

    Query optimization

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
+      @microposts_count = current_user.feed.count
       @pagy_feed, @feed_items = pagy(current_user.feed)
     end
   end
